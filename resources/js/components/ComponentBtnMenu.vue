f0ad374bf7e859436abcb491277c39284fdbae2a
Cleitinho69 <ezequielsantos2018@outlook.com.br>
2025-06-05 11:25:24 -0300

    .

--- resources/js/components/ComponentBtnMenu.vue
+++ resources/js/components/ComponentBtnMenu.vue
@@ -16,17 +16,12 @@
 </script>
 
 <template>
-<<<<<<< HEAD
-    <div id="btnMenu">
-    </div>
-=======
     <button id="btnMenu" @click="btnClick();$emit('clickMenu',active)" v-show="!active">
         <svg   viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H14M4 18H9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
     </button>
     <button id="btnMenuActive" @click="btnClick();$emit('clickMenu',active)" v-show="active" >
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_SM"> <path id="Vector" d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
     </button>
->>>>>>> 0e4ff3a (Menu criado e ajustado)
 </template>
 
 <style scoped>
@@ -47,15 +42,6 @@
         background: none;
         transition: 0.3s;
     }
-<<<<<<< HEAD
-</style>
-
-<script>
-    export default {
-        name: 'btnMenu',
-    }
-</script>
-=======
 
     
     #btnMenu svg{
@@ -89,4 +75,3 @@
 </style>
 
 
->>>>>>> 0e4ff3a (Menu criado e ajustado)
